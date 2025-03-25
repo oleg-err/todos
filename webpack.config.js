53f513daaa5101abd26ac7fb6438d0be78b25bcb
ereminoleg1@mail.ru <ereminoleg1@mail.ru>
2025-03-25 17:39:27 +0300

    Change output directory from dist to build

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default {
   entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     publicPath: './',
     filename: 'bundle.js',
   },
